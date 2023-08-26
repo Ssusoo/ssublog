5028945e9bfa5282aabe1d128e6f3795da3a8d23
zzanggoon8 <zzanggoon8@gmail.com>
2023-08-26 14:21:07 +0900

    first commit

---
+++ src/test/java/me/ssu/SsuApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ssu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
